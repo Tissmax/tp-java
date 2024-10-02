2543c6287ac12a7dcead0a3329684adfe3c45d33
Mathis <mathis.basier@gmail.com>
2024-10-02 12:12:03 +0200

    tp02

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,16 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = 0;
+        short b = 0;
+        int c = 0;
+        long d = 0;
+        float e = 0;
+        double f = 0;
+        char g = 0;
+        boolean h = true;
+        String i = "Voici le résultat d'un calcul:\n1+5=6";
+        System.out.println(i);
+    }
+}

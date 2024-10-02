4fe24945394faa9b29d710272446cb137da1adbf
Mathis <mathis.basier@gmail.com>
2024-10-02 14:17:18 +0200

    tp04

---
+++ src/fr/tableaux/ExerciceTableauChaines.java
@@ -0,0 +1,18 @@
+package fr.tableaux;
+
+public class ExerciceTableauChaines {
+    public static void main(String[] args) {
+        String[] a = {"Paris", "Bordeaux", "Pessac", "Lyon", "Nanterre"};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+        a = new String[]{"Paris", "Bordeaux", "Reims", "Lyon", "Nanterre"};
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+    }
+}

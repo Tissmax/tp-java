d7c07396131c3bf7fc5cb61a9e63ce0880101768
Mathis <mathis.basier@gmail.com>
2024-10-03 15:52:45 +0200

    tpAutonomnieFin

--- src/fr/algorithmie/InteractifFibonnaci.java
+++ src/fr/algorithmie/InteractifFibonnaci.java
@@ -1,5 +1,24 @@
 package fr.algorithmie;
 
+import java.util.Scanner;
+
 public class InteractifFibonnaci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Choisissez un nombre de rang N");
+        int n = scanner.nextInt();
+        int nbN1 = 1;
+        int nbN2 = 0;
+        int somme = 0;
+        if (n <= 1) {
+            System.out.println(n);
+        }
+        for (int i = 0; i < n; i++) {
+            somme = nbN1 + nbN2;
+            nbN2 = nbN1;
+            nbN1 = somme;
+        }
+        System.out.println(somme);
+    }
 
 }
